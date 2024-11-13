e0c203c595b9fa4aa15296e8aad2511b9e961401
Nathan <nathangoncalvesgomes@gmail.com>
2024-11-13 16:45:37 -0300

    Create 20de100.py

---
+++ 20de100.py
@@ -0,0 +1,8 @@
+# Lê um número inteiro
+numero = int(input("Digite um número inteiro: "))
+
+# Verifica se o número é par ou ímpar
+if numero % 2 == 0:
+    print(f"O número {numero} é PAR.")
+else:
+    print(f"O número {numero} é ÍMPAR.")
