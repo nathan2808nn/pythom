0a702d81f4ddf8a97215885156290cbba632e52a
Nathan <nathangoncalvesgomes@gmail.com>
2024-11-13 15:44:55 -0300

    Create 11de100.py

---
+++ 11de100.py
@@ -0,0 +1,8 @@
+# Solicita os coeficientes A, B e C da equação do segundo grau
+A = float(input("Digite o valor de A: "))
+B = float(input("Digite o valor de B: "))
+C = float(input("Digite o valor de C: "))
+# Calcula o valor de Delta
+delta = (B ** 2) - (4 * A * C)
+# Exibe o resultado
+print(f"O valor de Delta é {delta}.")
