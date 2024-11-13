bdcfac84138b9408cd70c12c3782cbcbd4fb8566
Nathan <nathangoncalvesgomes@gmail.com>
2024-11-13 16:46:15 -0300

    Create 21de100.py

---
+++ 21de100.py
@@ -0,0 +1,8 @@
+# Lê o ano
+ano = int(input("Digite o ano: "))
+
+# Verifica se o ano é bissexto
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+    print(f"O ano {ano} é bissexto.")
+else:
+    print(f"O ano {ano} não é bissexto.")
