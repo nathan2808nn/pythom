0a85cc1395bce5571ef7f1647ea7633b9abc542f
Nathan <nathangoncalvesgomes@gmail.com>
2024-11-13 16:50:41 -0300

    Create  26de100.py

---
+++ 26de100.py
@@ -0,0 +1,11 @@
+# Lê os dois números inteiros
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+# Compara os dois números e exibe a mensagem correspondente
+if num1 > num2:
+    print("O primeiro valor é o maior.")
+elif num2 > num1:
+    print("O segundo valor é o maior.")
+else:
+    print("Não existe valor maior, os dois são iguais.")
