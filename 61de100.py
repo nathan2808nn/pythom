e9cdccf97eef6ecf1d0372581b82ceca650eea73
Nathan <nathangoncalvesgomes@gmail.com>
2024-11-19 16:53:44 -0300

    Create 61de100.py

---
+++ 61de100.py
@@ -0,0 +1,10 @@
+# Inicializando o valor de contagem
+numero = 0
+
+# Estrutura "faça enquanto" (while) para a contagem
+while numero <= 30:
+    print(numero, end=" ")
+    numero += 3
+
+# Após a contagem, exibe "Acabou!"
+print("Acabou!")
