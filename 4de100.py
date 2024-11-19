f11d700154166c1aeedb9aeca5108090687dc1ca
Nathan <nathangoncalvesgomes@gmail.com>
2024-11-19 14:52:57 -0300

    Create 4de100.py

---
+++ 4de100.py
@@ -0,0 +1,11 @@
+# Lê o primeiro número inteiro
+numero1 = int(input("Digite o primeiro número: "))
+
+# Lê o segundo número inteiro
+numero2 = int(input("Digite o segundo número: "))
+
+# Calcula o somatório entre os dois números
+soma = numero1 + numero2
+
+# Exibe o resultado
+print(f"A soma entre {numero1} e {numero2} é: {soma}")
