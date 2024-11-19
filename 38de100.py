53d40264b0c01c7267383fec209eb67da70298c5
Nathan <nathangoncalvesgomes@gmail.com>
2024-11-19 14:38:46 -0300

    Create 38de100.py

---
+++ 38de100.py
@@ -0,0 +1,8 @@
+# Programa que exibe a contagem solicitada
+def main():
+    for i in range(6, 12):  # A contagem vai de 6 até 11
+        print(i, end=" ")  # Exibe o número seguido de um espaço
+    print("Acabou!")  # Exibe a palavra "Acabou!" no final
+
+# Chama a função principal
+main()
