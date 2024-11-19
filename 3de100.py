01ef435791ef23c62fcc12420245745132738e87
Nathan <nathangoncalvesgomes@gmail.com>
2024-11-19 14:38:02 -0300

    Create 3de100.py

---
+++ 3de100.py
@@ -0,0 +1,9 @@
+# Lê o nome do funcionário
+nome = input("Qual é o seu nome? ")
+
+# Lê o salário do funcionário
+salario = float(input("Qual é o seu salário? R$ "))
+
+# Exibe a mensagem com o nome e o salário
+print(f"Funcionário: {nome}")
+print(f"Salário: R$ {salario:.2f}")
