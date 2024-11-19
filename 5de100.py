ec34803bd799dfbb0d4e64bb25f562938da86db5
Nathan <nathangoncalvesgomes@gmail.com>
2024-11-19 16:52:46 -0300

    Create 5de100.py

---
+++ 5de100.py
@@ -0,0 +1,7 @@
+# Solicita as duas notas ao usuário
+nota1 = float(input("Nota 1: "))
+nota2 = float(input("Nota 2: "))
+# Calcula a média
+media = (nota1 + nota2) / 2
+# Exibe o resultado
+print(f"A média entre {nota1} e {nota2} é igual a {media:.1f}")
