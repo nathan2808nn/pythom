746d67c6369ff94e4d57b027be5b85a63e7f1158
Nathan <nathangoncalvesgomes@gmail.com>
2024-12-03 14:57:59 -0300

    Create atividade.py

---
+++ atividade.py
@@ -0,0 +1,10 @@
+import random
+
+# Definindo a lista com os nomes dos alunos
+alunos = ["Emanuel", "Nathan", "Lays", "Maria", "Lucas", "Beatriz", "Felipe"]
+
+# Sorteando um aluno da lista
+aluno_sorteado = random.choice(alunos)
+
+# Exibindo o nome do aluno sorteado
+print(f'O aluno sorteado foi: {aluno_sorteado}')
