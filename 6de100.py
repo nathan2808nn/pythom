d0c40c167fce10273eb274d6bd44416f789e393e
Nathan <nathangoncalvesgomes@gmail.com>
2024-12-03 14:41:26 -0300

    Create 6de100.py

---
+++ 6de100.py
@@ -0,0 +1,8 @@
+# Solicita um número inteiro ao usuário
+numero = int(input("Digite um número: "))
+# Calcula o antecessor e o sucessor
+antecessor = numero - 1
+sucessor = numero + 1
+# Exibe os resultados
+print(f"O antecessor de {numero} é {antecessor}")
+print(f"O sucessor de {numero} é {sucessor}")
